f5e217e66aaed32d60c2a17cfbf100915a803dfa
zhangwenle2026 <271846714@qq.com>
2026-03-29 12:24:46 -0300

    Add Service Worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'pt-v1';
+const ASSETS = ['/portuguese-starter/', '/portuguese-starter/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (!resp || resp.status !== 200) return resp;
+      var clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    }))
+  );
+});
